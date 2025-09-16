489abb3de2475411c307a3aa9818021201cc7f71
Pitambar majhi <pitambar@Pitambars-MacBook-Air.local>
2025-09-16 20:30:48 +0530

    logo change

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h2 className=' text-[44px] text-blue-600'>Hello</h2>
-      <h1 className=' text-[14px]'>Hello welcome</h1>
+      <div className=" text-[24px] text-amber-600 font-semibold text-center mt-5">
+        <h1 className=" ">
+          Hello welcome{" "}
+        </h1>
+        <h1 className=" ">to </h1>
+        <h1 className=" ">
+          Victoria falls Bed and Breakfast
+        </h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
