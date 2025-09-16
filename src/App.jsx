7b508db887c71f4d70a1d4f95427b17cb41af674
Pitambar majhi <pitambar@Pitambars-MacBook-Air.local>
2025-09-16 12:48:23 +0530

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className=' text-[44px] text-blue-600'>Hello</h2>
+      <h1 className=' text-[14px]'>Hello welcome</h1>
+    </>
+  )
+}
+
+export default App
