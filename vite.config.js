3cb357157b64b11876a73f5aaf8b653ac74ef6a5
Pitambar majhi <pitambar@Pitambars-MacBook-Air.local>
2025-09-23 21:49:38 +0530

    Home section

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  resolve: {
-    alias: {
-      '~slick-carousel': path.resolve(__dirname, 'node_modules/slick-carousel')
-    }
-  }
+  
 })
